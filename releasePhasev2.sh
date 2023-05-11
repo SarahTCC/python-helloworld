eb29e7cab359af791ee7eeb0faeb33f5586123ce
SarahTCC <sarah@thecommerce.co>
2023-05-11 14:38:10 +1000

    Trace

--- releasePhasev2.sh
+++ releasePhasev2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo $2
-curl -X POST -d api_key=somekey -d environment=staging -d sha=$2 https://app.sleuth.io/api/1/deployments/tcc/slidecart/register_deploy
+curl -X --trace POST -d api_key=somekey -d environment=staging -d sha=$2 https://app.sleuth.io/api/1/deployments/tcc/slidecart/register_deploy
